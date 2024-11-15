d0280a576a8e203fb36944bec7193568a71ef2bb
unknown <jihadmuntaha123@gmail.com>
2024-11-15 09:09:08 +0700

    uts web

---
+++ script.js
@@ -0,0 +1,7 @@
+function showAlert() {
+    alert('Mampu menjadi diri sendiri adalah salah satu komponen terkuat dari kesehatan mental yang baik!');
+}
+
+function notifikasi() {
+    alert('Terima Kasih atas Kritik dan Saran yang membangun!');
+}
